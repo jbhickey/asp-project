d6139857157bc9cbe19dc6099ec74ff1c1d3f7d4
Joshua Hickey <hickey.joshua@gmail.com>
2014-05-14 22:44:52 +1000

    Added readme and bad input argument protection

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installation
+
+`git clone https://github.com/jbhickey/asp_project.git`
+
+## Usage
+
+Run the program with filename and blocksize arguments
+`./codec Samples/test44100.wav 1024`
--- asp_project.c
+++ asp_project.c
@@ -12,38 +12,49 @@ char *write_buf;
 
 int main(int argc, char *argv[])
 {    
-	int block_size = atoi(argv[2]);
+	int block_size = 0;
 	int file_size = 0;
 	int block_cnt = 0;
 
-	error = (char*) malloc (sizeof(char)*block_size);
-	write_buf = (char*) malloc (sizeof(char)*block_size);
-	
-	// Filename as argument 
-	file_size = read_file(argv[1]);
-
-	// Write specifications to output file
-
-	// Iterate through input data
-	for(block_cnt=0;block_cnt<(file_size/block_size);block_cnt++)
+	if(argc != 3)
 	{
-		// Nth order avg predictor hence error[n-1]
-		avg_predictor(&g_data_buf[block_cnt*block_size], error, block_size);
-		
-		// Encode error vector
-		encode(error, write_buf, block_size);
-
-		// Write encoded data to file
-		write_block(write_buf, block_size);
-				
-		// Clear write buffer
-		memset(write_buf, 0x00, strlen(write_buf));
+		printf("Incorrect input arguments\n");
+		printf("Usage: ./codec filename blocksize\n"); 
+		printf("Example: ./codec Samples/test441100.wav 1024\n"); 
 	}
+	else
+	{
+		block_size = atoi(argv[2]);
 
-	free(error);
-	free(write_buf);
+		error = (char*) malloc (sizeof(char)*block_size);
+		write_buf = (char*) malloc (sizeof(char)*block_size);
+	
+		// Filename as argument 
+		file_size = read_file(argv[1]);
 
-	printf("Operation complete...\n");
+		// Write specifications to output file
+
+		// Iterate through input data
+		for(block_cnt=0;block_cnt<(file_size/block_size);block_cnt++)
+		{
+			// Nth order avg predictor hence error[n-1]
+			avg_predictor(&g_data_buf[block_cnt*block_size], error, block_size);
+		
+			// Encode error vector
+			encode(error, write_buf, block_size);
+
+			// Write encoded data to file
+			write_block(write_buf, block_size);
+				
+			// Clear write buffer
+			memset(write_buf, 0x00, strlen(write_buf));
+		}
+
+		free(error);
+		free(write_buf);
+
+		printf("Operation complete...\n");
+	}
 
 	return 0;
 }
