890b0f718f4b43429e1f2981dc2397392611c6ef
Joshua Hickey <hickey.joshua@gmail.com>
2014-06-01 21:25:45 +1000

    Removed optimum predictor

--- prediction.h
+++ prediction.h
@@ -2,9 +2,5 @@
 #define PREDICT_H
 
 void avg_predictor(char *s, char *e, int block_size);
-int optimum_predictor(char *signal_data, char *auto_corr_func, char *error);
-void auto_corr(char *s, char *r, long norm_factor);
-long get_norm_factor(char *s);
-void get_coeffs(int *a, char *r, int n);
 
 #endif
