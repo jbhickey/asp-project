c503b67261f25f54c8af36e94ebf11edc7b02b20
JBH <jhickey@arboraust.com>
2014-06-03 20:43:41 +1000

    update prediction with predictors according to lab sheet

--- prediction.c
+++ prediction.c
@@ -9,17 +9,16 @@ void avg_predictor(char *s, char *e, int block_size)
 	char *sp = (char*) malloc (sizeof(char)*block_size);
 	int n = 0;
 
-	// Iterate through signal and calculate prediction 
+	/* Iterate through signal and calculate prediction error*/
 	for(n=0;n<block_size;n++)
 	{
 		if(n==0)
-			sp[n] = s[n];
+			sp[n] = 0;
 		else if(n==1)
 			sp[n] = (s[n-1]/2);
 		else if(n>=2)
 			sp[n] = ((s[n-1]/2) + (s[n-2]/2));
-		
-		// Assign error for sample
+				
 		e[n] = s[n] - sp[n];		
 	}
 	free(sp);
@@ -34,20 +33,41 @@ void n_order_predictor(char *s, char *e, int block_size, int n_order)
 	switch(n_order){		
 	case 1:
 		for(n=0;n<block_size;n++){
-			sp[n] = s[n-1];
+			if(n==0){
+				sp[n] = 0;
+			} else {
+				sp[n] = s[n-1];
+			}
 		}
 		break;
 	case 2:
 		for(n=0;n<block_size;n++){
-			sp[n] = 2*s[n-1];
-			sp[n] -= s[n-2];
+			if(n==0){
+				sp[n] = 0;
+			} else if(n==1){	
+				sp[n] = 2*s[n-1];
+			} else {
+				sp[n] = 2*s[n-1];			
+				sp[n] -= s[n-2];
+			}
 		}
 		break;
 	case 3:
 		for(n=0;n<block_size;n++){
-			sp[n] = 3*s[n-1];
-			sp[n] -= 3*s[n-2];
-			sp[n] += s[n-3];
+			if(n==0){
+				sp[n] = 0;
+			}
+			if(n==1){
+				sp[n] = 3*s[n-1];
+			}
+			if(n==2){
+				sp[n] = 3*s[n-1];
+				sp[n] -= 3*s[n-2];
+			} else {
+				sp[n] = 3*s[n-1];
+				sp[n] -= 3*s[n-2];
+				sp[n] += s[n-3];
+			}
 		}
 		break;
 	default:
